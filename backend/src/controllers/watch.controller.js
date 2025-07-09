c626cb0353cec9d08ebc11de49efd19e5d11b892
Asciugano <samuele.diaferio@fastwebnet.it>
2025-07-09 19:56:33 +0200

    cambiato un po le route per i film e serie tv, ma non funziona

--- backend/src/controllers/watch.controller.js
+++ backend/src/controllers/watch.controller.js
@@ -40,35 +40,23 @@ export const getMoviesByPlatform = async (req, res) => {
 }
 
 export const getMoviesByGenre = async (req, res) => {
-  let { platforms, genres, content_types } = req.body;
+  // let { platforms, genres, content_types } = req.body;
 
   try {
-    if (!platforms || !Array.isArray(platforms) || platforms.length === 0)
-      return res.status(400).json({ message: 'You must specify at least 1 straming platform' });
-    if (!genres || !Array.isArray(genres) || genres.length === 0)
-      return res.status(400).json({ message: 'You must specify at least 1 genre' });
-
-    const response = await axios.get('https://imdb236.p.rapidapi.com/api/imdb/search', {
-      params: {
-        type: content_types || 'movie',
-        genres: genres,
-        rows: '25',
-        sortOrder: 'ASC',
-        sortField: 'id',
-      },
+    // if (!platforms || !Array.isArray(platforms) || platforms.length === 0)
+    //   return res.status(400).json({ message: 'You must specify at least 1 straming platform' });
+    // if (!genres || !Array.isArray(genres) || genres.length === 0)
+    //   return res.status(400).json({ message: 'You must specify at least 1 genre' });
+    //
+    const response = axios.get('https://api.themoviedb.org/3/movie/changes?page=1', {
       headers: {
-        'x-rapidai-key': process.env.RAPID_API_KEY,
-        'x-rapidapi-host': 'imdb236.p.rapidapi.com',
-      }
+        Accept: 'application/json',
+        Authorization: `Bearer ${process.env.MOVIEDB_HEADER}`,
+      },
     });
 
-    res.status(200).json({
-      results: response.data.map(item => ({
-        primaryTitle: item.primaryTitle,
-        description: item.description,
-        primaruImage: item.primaruImage,
-      }))
-    });
+    res.status(200).json(response);
+
   } catch (e) {
     console.log('Error in getMoviesByGenre', e.message);
     res.status(500).json({ message: 'Internal Server Error', error: e.message });
